57b6b34fe01cdbaa5f6a71f33e5ff2631fa1c858
garam2024 <76989920+garam2024@users.noreply.github.com>
2021-12-14 22:19:46 +0900

    commit

---
+++ callPage-1/src/main/java/com/geno/callPage/CallPage1Application.java
@@ -0,0 +1,14 @@
+package com.geno.callPage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallPage1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallPage1Application.class, args);
+	}
+
+}
+//æ»≥Á«œººø‰ 
